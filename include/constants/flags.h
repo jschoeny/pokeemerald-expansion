e4c271c0550fa5fa7efc4a6dfba87062ce8ea3d7
jschoeny <jared@schoeny.com>
2022-07-12 13:13:22 -0400

    Hidden daily Mysterious Shard in Lilycove Harbor

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1579,7 +1579,7 @@
 #define FLAG_UNUSED_0x924                           (DAILY_FLAGS_START + 0x4)  // Unused Flag
 #define FLAG_UNUSED_0x925                           (DAILY_FLAGS_START + 0x5)  // Unused Flag
 #define FLAG_UNUSED_0x926                           (DAILY_FLAGS_START + 0x6)  // Unused Flag
-#define FLAG_UNUSED_0x927                           (DAILY_FLAGS_START + 0x7)  // Unused Flag
+#define FLAG_DAILY_HIDDEN_MYSTERIOUS_SHARD          (DAILY_FLAGS_START + 0x7)
 #define FLAG_DAILY_SLATE                            (DAILY_FLAGS_START + 0x8)
 #define FLAG_DAILY_OUTBREAK                         (DAILY_FLAGS_START + 0x9)
 #define FLAG_DAILY_PICKED_LOTO_TICKET               (DAILY_FLAGS_START + 0xA)
